b2e558e14c8dc8017ef9268c3c203097ba657b51
fossilfriend <egreenfest@gmail.com>
2024-11-05 12:23:00 -0500

    add text response handler

--- utils/backend.ts
+++ utils/backend.ts
@@ -16,3 +16,13 @@ export async function backendFetchFromPath(path: string) {
     const data = await response.json()
     return data
 }
+
+export async function backendFetchText(request: NextRequest) {
+    const incomingRequestUrl = new URL(request.url)
+    const path = incomingRequestUrl.pathname
+    const queryParams = incomingRequestUrl.search
+    const requestUri:string = process.env.API_SERVICE_URL! + path + queryParams
+    const response = await(fetch(requestUri))
+    const data = await response.text()
+    return data
+}
