0e46a67055d3284343f81ca3143ed71bc01c0ef3
fossilfriend <egreenfest@gmail.com>
2024-11-05 15:53:56 -0700

    use proxypass value for redirecting views

--- app/filer/route.ts
+++ app/filer/route.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import { backendFetch } from '@/utils/backend'
 
 export async function GET(request: NextRequest) {
+    const requestUrl = process.env.API_PUBLIC_URL;
     const response = await backendFetch(request)
     if (response.hasOwnProperty('redirect')) {
         const redirectEndpoint = `${response['redirect']}/${response['queryId']}`
-        const redirectUrl = new URL(redirectEndpoint, request.url)
+        const redirectUrl = new URL(redirectEndpoint, requestUrl)
         return NextResponse.redirect(redirectUrl)
     }
 
--- app/openapi.json/route.ts
+++ app/openapi.json/route.ts
@@ -3,6 +3,5 @@ import { backendFetch } from '@/utils/backend'
 
 export async function GET(request: NextRequest) {
     const response = await backendFetch(request)
-    console.log(response)
     return Response.json(response)
 }
