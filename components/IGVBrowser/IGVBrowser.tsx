73dbd1c7809b0f754dc3decf8429dd86e2bc18db
fossilfriend <egreenfest@gmail.com>
2024-11-05 23:02:38 -0700

    useRef instead of document/window

--- components/IGVBrowser/IGVBrowser.tsx
+++ components/IGVBrowser/IGVBrowser.tsx
@@ -1,15 +1,27 @@
-'use client'
-import React, { useLayoutEffect, useMemo, useState, useEffect } from "react";
+"use client";
+
+// TODO: streaming w/Suspense and fallback instead
+
+import React, {
+    useLayoutEffect,
+    useMemo,
+    useState,
+    useEffect,
+    useRef,
+} from "react";
 import igv from "igv/dist/igv.esm";
 import noop from "lodash.noop";
 import find from "lodash.find";
 import {
     VariantPValueTrack,
     VariantServiceTrack as VariantTrack,
-    trackPopover
+    trackPopover,
 } from "@/components/IGVBrowser/tracks";
 import { _genomes } from "@/components/IGVBrowser/data/_igvGenomes";
-import { Session, TrackBaseOptions } from "@/components/IGVBrowser/types/tracks";
+import {
+    Session,
+    TrackBaseOptions,
+} from "@/components/IGVBrowser/types/tracks";
 import {
     loadTracks,
     createSessionSaveObj,
@@ -17,13 +29,9 @@ import {
     getLoadedTracks,
     removeTrackById,
 } from "@/components/IGVBrowser/utils/index";
-import LoadSession from "./LoadSession";
-import SaveSession from "./SaveSession";
-
-export const DEFAULT_FLANK = 1000;
+import { DEFAULT_FLANK, FEATURE_SEARCH_ENDPOINT } from "./data/_constants";
 
 interface IGVBrowserProps {
-    featureSearchUrl: string;
     genome: string;
     locus?: string;
     onTrackRemoved?: (track: string) => void;
@@ -32,7 +40,6 @@ interface IGVBrowserProps {
 }
 
 const IGVBrowser: React.FC<IGVBrowserProps> = ({
-    featureSearchUrl,
     genome,
     locus,
     onBrowserLoad,
@@ -42,6 +49,8 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
     const [browserIsLoaded, setBrowserIsLoaded] = useState<boolean>(false);
     const [browser, setBrowser] = useState<any>(null);
     const [sessionJSON, setSessionJSON] = useState<Session>({ tracks: tracks });
+    const [isClient, setIsClient] = useState(false);
+    const containerRef = useRef(null);
 
     const memoOptions: any = useMemo(() => {
         const referenceTrackConfig: any = find(_genomes, { id: genome });
@@ -51,7 +60,7 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
             flanking: DEFAULT_FLANK,
             minimumBases: 40,
             search: {
-                url: `${featureSearchUrl}$FEATURE$&flank=${DEFAULT_FLANK}`,
+                url: `${FEATURE_SEARCH_ENDPOINT}$FEATURE$&flank=${DEFAULT_FLANK}`,
             },
             reference: {
                 id: genome,
@@ -66,7 +75,11 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
         };
     }, [genome, locus]);
 
-    useLayoutEffect(() => {
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    useEffect(() => {
         // setting initial session due to component load/reload
         if (browserIsLoaded && memoOptions && tracks) {
             const loadedTracks = getLoadedTracks(browser);
@@ -81,40 +94,49 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
             // load initial tracks
             loadTracks(tracks, browser);
         }
-    }, [browserIsLoaded, memoOptions, tracks]);
+    }, [browserIsLoaded]);
 
     useLayoutEffect(() => {
         /* window.addEventListener("ERROR: Genome Browser - ", (event) => {
             console.log(event);
         }); */
 
-        const targetDiv = document.getElementById("genome-browser");
+        if (isClient && containerRef.current) {
+            const targetDiv = containerRef.current;
+            // const targetDiv = document.getElementById("genome-browser");
 
-        if (memoOptions != null) {
-            igv.registerTrackClass("gwas_service", VariantPValueTrack);
-            igv.registerTrackClass("eqtl", VariantPValueTrack);
-            igv.registerTrackClass("variant_service", VariantTrack);
+            if (memoOptions != null) {
+                igv.registerTrackClass("gwas_service", VariantPValueTrack);
+                igv.registerTrackClass("eqtl", VariantPValueTrack);
+                igv.registerTrackClass("variant_service", VariantTrack);
 
-            igv.createBrowser(targetDiv, memoOptions).then(function (
-                browser: any
-            ) {
-                // custom track popovers
-                browser.on("trackclick", trackPopover);
+                igv.createBrowser(targetDiv, memoOptions).then(function (
+                    browser: any
+                ) {
+                    // custom track popovers
+                    browser.on("trackclick", trackPopover);
 
-                // perform action in encapsulating component if track is removed
-                browser.on("trackremoved", function (track: any) {
-                    if (onTrackRemoved) {onTrackRemoved(track.config.id);}
+                    // perform action in encapsulating component if track is removed
+                    browser.on("trackremoved", function (track: any) {
+                        if (onTrackRemoved) {
+                            onTrackRemoved(track.config.id);
+                        }
+                    });
+
+                    // add browser to state
+                    setBrowser(browser);
+                    setBrowserIsLoaded(true);
+
+                    // callback to parent component, if exist
+                    if (onBrowserLoad) {
+                        onBrowserLoad(browser);
+                    } else {
+                        noop();
+                    }
                 });
-
-                // add browser to state
-                setBrowser(browser);
-                setBrowserIsLoaded(true);
-
-                // callback to parent component, if exist
-                if (onBrowserLoad) { onBrowserLoad(browser);} else {noop();}
-            });
+            }
         }
-    }, [onBrowserLoad, memoOptions]);
+    }, [isClient]);
 
     //rearrange
     const handleSaveSession = () => {
@@ -126,9 +148,17 @@ const IGVBrowser: React.FC<IGVBrowserProps> = ({
         }
     };
 
+    if (!isClient) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
-            <span style={{ width: "100%" }} id="genome-browser" />
+            <span
+                ref={containerRef}
+                style={{ width: "100%" }}
+                id="genome-browser"
+            />
         </>
     );
 };
